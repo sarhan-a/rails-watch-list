395ac86bf9911940a0f12d114513112fc74d0b50
Sarhan Alim <sarhan.alim@gmail.com>
2022-07-23 15:54:15 +0100

    add bookmark model and references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_23_143743) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_144835) do
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.integer "movie_id", null: false
+    t.integer "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
+
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -26,4 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_23_143743) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
